bdae47c830731ce08d0aa250207978cd04bf2714
archer <econarcher2010@gmail.com>
2024-11-09 21:19:18 +0800

    add WorkoutDetails comp.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <header>
-      <div className="container"></div>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+      </div>
     </header>
   );
 }
