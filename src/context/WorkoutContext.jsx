2dfa47bd79434cfa8d563ec671aaa908d5f13d99
archer <econarcher2010@gmail.com>
2024-11-10 21:20:53 +0800

    separate WorkoutsReducer

--- src/context/WorkoutContext.jsx
+++ src/context/WorkoutContext.jsx
@@ -1,29 +1,9 @@
 import { createContext, useReducer } from "react";
+import { workoutsReducer } from "./WorkoutsReducer";
 
 export const WorkoutsContext = createContext();
 export const API_URL =
   "https://workout-backend-2hqj.onrender.com/api/workouts/";
-
-export const workoutsReducer = (state, action) => {
-  switch (action.type) {
-    case "SET_WORKOUTS":
-      return {
-        workouts: action.payload,
-      };
-    case "CREATE_WORKOUT":
-      return {
-        workouts: [action.payload, ...state.workouts],
-      };
-    case "DELETE_WORKOUT":
-      return {
-        workouts: state.workouts.filter((workout) => {
-          return workout._id !== action.payload._id;
-        }),
-      };
-    default:
-      return state;
-  }
-};
 
 export const WorkoutsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(workoutsReducer, {
