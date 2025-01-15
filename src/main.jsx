3868fd0d246d944f88dac93daa1cb5c45c4ed0d2
archer <econarcher2010@gmail.com>
2025-01-15 15:18:18 +0800

    React Auth Context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { WorkoutsContextProvider } from "./context/WorkoutContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </StrictMode>
 );
